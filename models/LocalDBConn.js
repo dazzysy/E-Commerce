e67c87e6442c345f0b02c33b0380d0cffac1c7b1
dazzysy <shuyi.yang@huskers.unl.edu>
2018-05-03 18:21:11 -0500

    把连接的数据库换成azure的数据库

--- models/LocalDBConn.js
+++ models/LocalDBConn.js
@@ -3,8 +3,8 @@
  */
 let Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('onlineshoppingdb', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('onlineshoppingdb', 'dazzysy@dazzecommerceback-mysqldbserver', 'Mima@123', {
+    host: 'dazzecommerceback-mysqldbserver.mysql.database.azure.com',
     dialect: 'mysql',
     pool:{
         max: 5,
